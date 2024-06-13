811900653868ac52e61da7f8265e3f5839f8f8f7
manassetimoteo04 <manassetimoteo4@gmail.com>
2024-06-13 22:30:38 +0100

    New commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
 import { admin } from "./admin.js";
 feather.replace();
+
+class mainApp {
+  constructor() {
+    this.btnToggleMenu = document.querySelector(".btn-toggle-menu");
+    this.btnToggleMenu?.addEventListener("click", this._toggleMenu.bind(this));
+    this.header = document.querySelector(".logo-heading");
+    console.log(this.header);
+    this._obserserAddSticky();
+  }
+  _toggleMenu() {
+    const body = document.querySelector("body");
+    body.classList.toggle("show-menu");
+  }
+  _obserserAddSticky() {
+    const headerObserver = new IntersectionObserver(this._observerCallBack, {
+      root: null,
+      threshold: 0,
+    });
+    headerObserver.observe(this.header);
+  }
+  _observerCallBack(entries) {
+    const body = document.querySelector("body");
+    const [entry] = entries;
+    if (!entry.isIntersecting) {
+      body.classList.add("sticky");
+    } else {
+      body.classList.remove("sticky");
+    }
+  }
+}
+const app = new mainApp();
